e4b0b1ecf3d1a6fea041b930d912509902a691f2
tentabs00 <tentabs00@gmail.com>
2024-09-10 11:35:47 -0700

    variables and configs

--- tests/test_training_data.py
+++ tests/test_training_data.py
@@ -431,7 +431,6 @@ def load_test_config():
     with open(config_path, 'r', encoding='utf-8') as f:
         config = yaml.safe_load(f)
     return config
-
 config = load_test_config()
 
 # Module-level variables
