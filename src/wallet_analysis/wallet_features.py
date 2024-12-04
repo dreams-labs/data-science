b96d31ebd2f8f9539782c92e0ea1a74f77801ae1
tentabs00 <tentabs00@gmail.com>
2024-12-04 11:50:04 -0800

    base model working

--- src/wallet_analysis/wallet_features.py
+++ src/wallet_analysis/wallet_features.py
@@ -1,21 +1,21 @@
 '''
 calculates metrics related to the distribution of coin ownership across wallets
 '''
-# pylint: disable=C0301 # line too long
 
-import pandas as pd
+import time
 import dreams_core.core as dc
 
 # set up logger at the module level
 logger = dc.setup_logger()
 
 
-def calculate_wallet_investment_return(profits_df):
+def calculate_wallet_level_metrics(profits_df):
     """
-    Calculates the return on investment for the wallet, defined below.
+    Calculates the return on investment for the wallet (defined below) and
+    additional aggregation metrics.
 
-    Profits_df must have initial balances reflected as positive usd_net_transfers
-    and ending balances reflected as negative usd_net_transfers for calculations to
+    Profits_df must have initial balances reflected as positive cash_flow_transfers
+    and ending balances reflected as negative cash_flow_transfers for calculations to
     be accurately reflected.
 
     - Invested: the maximum amount of cumulative net inflows for the wallet. This
@@ -29,30 +29,38 @@ def calculate_wallet_investment_return(profits_df):
         with columns coin_id,wallet_address,date
 
     Returns:
-    - wallet_performance_df (pd.DataFrame): df keyed on wallet_address with columns
-        'ingested', 'net_gain' , 'return'
+    - wallet_metrics_df (pd.DataFrame): df keyed on wallet_address with columns
+        'ingested', 'net_gain' , 'return', and additional aggregation metrics
     """
+    start_time = time.time()
 
-    profits_df = profits_df.set_index('wallet_address')
+    # Precompute necessary transformations
+    profits_df['abs_usd_net_transfers'] = profits_df['usd_net_transfers'].abs()
+    profits_df['cumsum_cash_flow_transfers'] = profits_df.groupby('wallet_address')['cash_flow_transfers'].cumsum()
 
-    # Calculate amount invested
-    wallet_invested_df = pd.DataFrame(
-        profits_df
-        .groupby(level='wallet_address')['usd_net_transfers'].cumsum()
-        .groupby(level='wallet_address').max()
+    # Metrics that take into account imputed rows/profits
+    logger.debug("Calculating wallet metrics based on imputed performance...")
+    imputed_metrics_df = profits_df.groupby('wallet_address').agg(
+        invested=('cumsum_cash_flow_transfers', 'max'),
+        net_gain=('cash_flow_transfers', lambda x: -1 * x.sum()),  # outflows reflect profit-taking
+        unique_coins_held=('coin_id', 'nunique')
     )
-    wallet_invested_df.columns = ['invested']
 
-    # Calculate net gains
-    wallet_gain_df = pd.DataFrame(
-        -profits_df.groupby(level='wallet_address')['usd_net_transfers'].sum()
+    # Metrics only based on observed activity
+    logger.debug("Calculating wallet metrics based on observed behavior...")
+    observed_metrics_df = profits_df[~profits_df['is_imputed']].groupby('wallet_address').agg(
+        transaction_days=('date', 'count'),
+        total_volume=('abs_usd_net_transfers', 'sum'),
+        average_transaction=('abs_usd_net_transfers', 'mean')
     )
-    wallet_gain_df.columns = ['net_gain']
 
-    # Join dfs
-    wallet_performance_df = wallet_invested_df.join(wallet_gain_df)
+    # Join all metrics together
+    wallet_metrics_df = imputed_metrics_df.join(observed_metrics_df)
 
-    # Compute return
-    wallet_performance_df['return'] = wallet_performance_df['net_gain']/wallet_performance_df['invested']
+    # Compute additional derived metrics
+    wallet_metrics_df['return'] = wallet_metrics_df['net_gain'] / wallet_metrics_df['invested']
 
-    return wallet_performance_df
+    logger.debug("Wallet metric computation complete after %.2f.", start_time-time.time())
+
+
+    return wallet_metrics_df
