ce89f7269bb89f77e6346e6bcd0dbb002c2fa1bd
tentabs00 <tentabs00@gmail.com>
2024-12-06 22:53:42 -0800

    wallets config manager

--- src/wallet_analysis/wallet_training_data.py
+++ src/wallet_analysis/wallet_training_data.py
@@ -1,5 +1,5 @@
 """
-Utility functions used as part of the wallet modeling pipeline
+Primary sequence functions used as part of the wallet modeling pipeline
 """
 
 from datetime import datetime,timedelta
@@ -8,18 +8,60 @@ import numpy as np
 import pandas_gbq
 import dreams_core.core as dc
 
-# set up logger at the module level
+# Local module imports
+# pylint:disable=E0401  # can't find local imports
+from wallet_analysis.wallets_config_manager import WalletsConfig
+
+# Set up logger at the module level
 logger = dc.setup_logger()
 
+# Load wallets_config at the module level
+wallets_config = WalletsConfig()
 
-def apply_wallet_thresholds(wallet_metrics_df, wallets_config):
+
+def generate_imputation_dates():
+    """
+    Generates a list of all dates that need imputation, including the first
+    and last date of each training window and modeling period.
+
+    The training period boundary dates are returned separateely so they can be used
+    to
+
+    Returns:
+    - imputation_dates (list): list that includes all start and end dates
+    """
+    # Extract all window start dates
+    window_start_dates = sorted([datetime.strptime(date, "%Y-%m-%d")
+                    for date in wallets_config['training_data']['training_window_starts'].values()
+                    ])
+
+    # Generate the output array
+    imputation_dates = [window_start_dates[0].strftime("%Y-%m-%d")]  # Include the first date
+    for i in range(1, len(window_start_dates)):
+        # Append the day before each window start
+        imputation_dates.append((window_start_dates[i] - timedelta(days=1)).strftime("%Y-%m-%d"))
+        # Append window start date
+        imputation_dates.append(window_start_dates[i].strftime("%Y-%m-%d"))
+
+    # Append day before modeling period
+    modeling_start = datetime.strptime(wallets_config['training_data']['modeling_period_start'], "%Y-%m-%d")
+    imputation_dates.append((modeling_start - timedelta(days=1)).strftime("%Y-%m-%d"))
+
+    # Append modeling period dates
+    imputation_dates.append(wallets_config['training_data']['modeling_period_start'])
+    imputation_dates.append(wallets_config['training_data']['modeling_period_end'])
+
+    return imputation_dates
+
+
+
+def apply_wallet_thresholds(wallet_metrics_df):
     """
     Applies data cleaning filters to the a df keyed on wallet_address
 
     Params:
     - wallet_metrics_df (df): dataframe with index wallet_address and columns with
         metrics that the filters will be applied to
-    - wallets_config (dict): dict with data cleaning filter levels
     """
     # inflows_filter flagged wallets
     excess_inflows_wallets = wallet_metrics_df[
@@ -56,40 +98,57 @@ def apply_wallet_thresholds(wallet_metrics_df, wallets_config):
 
 
 
-def generate_imputation_dates(wallets_config):
+def split_window_dfs(windows_profits_df):
     """
-    Generates a list of all dates that need imputation, including the first
-    and last date of each training and modeling period.
+    Splits the full profits_df into separate dfs for each training window and the modeling period.
 
     Params:
-    - wallets_config (dict): config with relevant training period and modeling period dates
+    - windows_profits_df (df): dataframe containing profits data for all periods, with imputed rows
+        for each period start and end
 
     Returns:
-    - imputation_dates (list): list that includes the start and end dates of each period
+    - training_windows_dfs (list of dfs): list of profits_dfs for each training window
+    - modeling_period_df (df): profits_df for the modeling period only
     """
+    # Extract modeling period boundaries
+    modeling_period_start = datetime.strptime(wallets_config['training_data']['modeling_period_start'], "%Y-%m-%d")
+    modeling_period_end = datetime.strptime(wallets_config['training_data']['modeling_period_end'], "%Y-%m-%d")
 
-    # Extract all period start dates
-    period_start_dates = sorted([datetime.strptime(date, "%Y-%m-%d")
-                    for date in wallets_config['training_data']['training_period_starts'].values()
-                    ])
+    # Convert training window starts to sorted datetime
+    training_windows_starts = sorted(
+        [datetime.strptime(date, "%Y-%m-%d") for date in wallets_config['training_data']['training_window_starts'].values()]
+    )
 
-    # Generate the output array
-    imputation_dates = [period_start_dates[0].strftime("%Y-%m-%d")]  # Include the first date
-    for i in range(1, len(period_start_dates)):
-        # Append the day before each period start
-        imputation_dates.append((period_start_dates[i] - timedelta(days=1)).strftime("%Y-%m-%d"))
-        # Append period start date
-        imputation_dates.append(period_start_dates[i].strftime("%Y-%m-%d"))
+    # Generate end dates for each period
+    training_windows_ends = training_windows_starts[1:] + [modeling_period_end - pd.Timedelta(days=1)]
 
-    # Append day before modeling period
-    modeling_start = datetime.strptime(wallets_config['training_data']['modeling_period_start'], "%Y-%m-%d")
-    imputation_dates.append((modeling_start - timedelta(days=1)).strftime("%Y-%m-%d"))
+    # Create array of DataFrames for each training period
+    training_windows_dfs = []
+    for start, end in zip(training_windows_starts, training_windows_ends):
+        window_df = windows_profits_df[
+            (windows_profits_df['date'] >= start) & (windows_profits_df['date'] < end)
+        ]
+        training_windows_dfs.append(window_df)
 
-    # Append modeling period dates
-    imputation_dates.append(wallets_config['training_data']['modeling_period_start'])
-    imputation_dates.append(wallets_config['training_data']['modeling_period_end'])
+    # Result: array of DataFrames
+    for i, df in enumerate(training_windows_dfs):
+        logger.info("Training Window %s (%s to %s): %s",
+                    i + 1,
+                    training_windows_starts[i].strftime('%Y-%m-%d'),
+                    training_windows_ends[i].strftime('%Y-%m-%d'),
+                    df.shape)
 
-    return imputation_dates
+    # Extract modeling period DataFrame
+    modeling_period_df = windows_profits_df[
+            (windows_profits_df['date'] >= modeling_period_start) & (windows_profits_df['date'] < modeling_period_end)
+
+        ]
+    logger.info("Modeling Period (%s to %s): %s",
+                modeling_period_start.strftime('%Y-%m-%d'),
+                modeling_period_end.strftime('%Y-%m-%d'),
+                modeling_period_df.shape)
+
+    return training_windows_dfs, modeling_period_df
 
 
 
