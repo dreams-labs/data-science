9593a55efdc965c0560f33488e117a814d6222df
tentabs00 <tentabs00@gmail.com>
2025-05-07 22:07:21 -0700

    feature: TimeWindowsOrchestrator() fully online

--- src/feature_engineering/time_windows_orchestration.py
+++ src/feature_engineering/time_windows_orchestration.py
@@ -127,10 +127,8 @@ class TimeWindowsOrchestrator:
             all_flattened_filepaths.extend(window_flattened_filepaths)
 
         # 3. Combine features & targets
-        concatenated_dfs = concat_dataset_time_windows_dfs(
-            all_flattened_filepaths, self.modeling_config
-        )
-        training_data_df, join_logs_df = join_dataset_all_windows_dfs(concatenated_dfs)
+        concatenated_dfs = self._concat_dataset_time_windows_dfs(all_flattened_filepaths)
+        training_data_df, join_logs_df = self._join_dataset_all_windows_dfs(concatenated_dfs)
 
         u.notify('calendar_reminder')
         self.logger.info(
@@ -210,6 +208,7 @@ class TimeWindowsOrchestrator:
         market_data_df = ind.add_market_data_dualcolumn_indicators(market_data_df)
 
         return macro_trends_df, market_data_df, profits_df, prices_df
+
 
 
     def _generate_time_windows(self):
@@ -296,186 +295,185 @@ class TimeWindowsOrchestrator:
 
 
 
-def concat_dataset_time_windows_dfs(
-        filepaths: List[str],
-        modeling_config: Dict
-    ) -> Dict[str, Tuple[pd.DataFrame, str]]:
-    """
-    For each dataset, concatenates the flattened dfs from all windows together and returns a
-    tuple containing the merged df with its fill method.
+    def _concat_dataset_time_windows_dfs(
+            self,
+            filepaths: List[str],
+        ) -> Dict[str, Tuple[pd.DataFrame, str]]:
+        """
+        For each dataset, concatenates the flattened dfs from all windows together and returns a
+        tuple containing the merged df with its fill method.
 
-    This function reads multiple CSV files, extracts their dataset prefixes,
-    concatenates DataFrames with the same prefix, and determines the fill method for each dataset.
-    It handles subgroups within datasets by prefixing column names with the subgroup name.
+        This function reads multiple CSV files, extracts their dataset prefixes,
+        concatenates DataFrames with the same prefix, and determines the fill method for each dataset.
+        It handles subgroups within datasets by prefixing column names with the subgroup name.
 
-    Args:
-        filepaths (List[str]): A list of file paths to the CSV files to be processed.
-        modeling_config (Dict): Configuration dictionary containing preprocessing fill methods.
+        Args:
+        - filepaths (List[str]): A list of file paths to the CSV files to be processed.
 
-    Returns:
-        concatenated_dfs (Dict[str, Tuple[pd.DataFrame, str]]): A dictionary where keys are
-        dataset prefixes and values are tuples containing:
-            - The concatenated DataFrame for each prefix
-            - The fill method for the dataset
-    """
-    # Dictionary to store DataFrames grouped by dataset prefix
-    grouped_dfs = {}
-    fill_methods = {}
+        Returns:
+        - concatenated_dfs (Dict[str, Tuple[pd.DataFrame, str]]): A dictionary where keys are
+            dataset prefixes and values are tuples containing:
+             - The concatenated DataFrame for each prefix
+             - The fill method for the dataset
+        """
+        # Dictionary to store DataFrames grouped by dataset prefix
+        grouped_dfs = {}
+        fill_methods = {}
 
-    parent_directory = os.path.join(modeling_config['modeling']['modeling_folder'],
-                                    'outputs/preprocessed_outputs/')
+        parent_directory = os.path.join(self.modeling_config['modeling']['modeling_folder'],
+                                        'outputs/preprocessed_outputs/')
 
-    for filepath in filepaths:
-        # Validate file existence
-        if not os.path.exists(filepath):
-            raise KeyError(f"File {filepath} does not exist.")
+        for filepath in filepaths:
+            # Validate file existence
+            if not os.path.exists(filepath):
+                raise KeyError(f"File {filepath} does not exist.")
 
-        # Extract the dataset prefix from the filename
-        dataset_prefix = extract_dataset_key_from_filepath(filepath, parent_directory)
+            # Extract the dataset prefix from the filename
+            dataset_prefix = self._extract_dataset_key_from_filepath(filepath, parent_directory)
 
-        # Read the CSV
-        df = pd.read_csv(filepath)
+            # Read the CSV
+            df = pd.read_csv(filepath)
 
-        # Handle dataset subkeys if present
-        if '-' in dataset_prefix:
-            dataset, dataset_subkey = dataset_prefix.split('-')  # pylint: disable=W0612
-            # Prefix the columns with the subgroup
+            # Handle dataset subkeys if present
+            if '-' in dataset_prefix:
+                dataset, dataset_subkey = dataset_prefix.split('-')  # pylint: disable=W0612
+                # Prefix the columns with the subgroup
+            else:
+                dataset = dataset_prefix
+
+            # Add the DataFrame to the appropriate group
+            if dataset_prefix not in grouped_dfs:
+
+                grouped_dfs[dataset_prefix] = []
+                # Identify the fill method only once per dataset
+                try:
+                    fill_methods[dataset_prefix] = self.modeling_config['preprocessing']['fill_methods'][dataset]
+                except KeyError as exc:
+                    raise KeyError(f"Fill method not found for dataset: {dataset}") from exc
+
+            grouped_dfs[dataset_prefix].append(df)
+
+        # Concatenate DataFrames within each group and pair with fill method
+        concatenated_dfs = {
+            dataset_prefix: (pd.concat(dfs, ignore_index=True), fill_methods[dataset_prefix])
+            for dataset_prefix, dfs in grouped_dfs.items()
+        }
+
+        return concatenated_dfs
+
+
+    @staticmethod
+    def _join_dataset_all_windows_dfs(concatenated_dfs):
+        """
+        Merges the all-windows dataframes of each dataset together according to the fill method
+        specified in the model_config. The param is the format from tw.concat_dataset_time_windows_dfs().
+
+        Params:
+        - concatenated_dfs (Dict[str, Tuple[pd.DataFrame, str]]): A dictionary where keys are
+            dataset prefixes and values are tuples containing:
+                - The concatenated DataFrame for each prefix
+                - The fill method for the dataset
+
+        Returns
+        - training_data_df (DataFrame): df multiindexed on time_window-coin_id, with columns for
+            all flattened features for all datasets.
+        - join_log_df (DataFrame): df that contains briefs logs of join methods and outcomes
+        """
+        # List to store all combinations
+        all_coin_windows = []
+        log_data = []
+
+        # Pull a unique set of all coin_id-time_window pairs across all dfs
+        for dataset, (df, _) in concatenated_dfs.items():
+            if 'coin_id' in df.columns and 'time_window' in df.columns:
+                combinations = df[['time_window', 'coin_id']].drop_duplicates()
+                all_coin_windows.append(combinations)
+
+        # Concatenate all combinations and remove duplicates
+        training_data_df = pd.concat(all_coin_windows, ignore_index=True).drop_duplicates()
+        training_data_df = training_data_df.set_index(['time_window', 'coin_id'])
+
+        # Iterate through df_list and merge each one
+        for dataset, (df, fill_method) in concatenated_dfs.items():
+            rows_start = len(training_data_df)
+
+            # Get columns to track from current df so we can fill them appropriately
+            merge_cols = df.columns.difference(['time_window', 'coin_id'])
+
+            if fill_method == 'retain_nulls':
+                df = df.set_index(['time_window', 'coin_id'])
+                training_data_df = training_data_df.join(df, on=['time_window', 'coin_id'], how='left')
+
+            elif fill_method == 'drop_records':
+                df = df.set_index(['time_window', 'coin_id'])
+                df = df.dropna()
+                training_data_df = training_data_df.join(df, on=['time_window', 'coin_id'], how='inner')
+
+            elif fill_method == 'fill_zeros':
+                df = df.set_index(['time_window', 'coin_id'])
+                training_data_df = training_data_df.join(df, on=['time_window', 'coin_id'], how='left')
+                # Only fill the columns from current df
+                training_data_df[merge_cols] = training_data_df[merge_cols].fillna(0)
+
+            elif fill_method == 'extend_coin_ids':
+                df = df.set_index('time_window')
+                training_data_df = training_data_df.join(df, on='time_window', how='inner')
+
+            elif fill_method == 'extend_time_windows':
+                df = df.set_index('coin_id')
+                training_data_df = training_data_df.join(df, on='coin_id', how='inner')
+
+            else:
+                raise ValueError(f"Invalid fill method '{fill_method}' found in config.yaml.")
+
+            rows_end = len(training_data_df)
+            log_data.append({
+                'dataset': dataset,
+                'fill_method': fill_method,
+                'rows_start': rows_start,
+                'rows_end': rows_end,
+                'rows_removed': rows_start - rows_end
+            })
+
+        join_log_df = pd.DataFrame(log_data)
+
+        return training_data_df, join_log_df
+
+
+
+    @staticmethod
+    def _extract_dataset_key_from_filepath(filepath, parent_directory):
+        """
+        Helper function for concat_dataset_time_windows_dfs().
+
+        Extracts the dataset key (e.g. 'time_series', 'wallet_cohorts', etc) from the
+        flattened filepath.
+
+        Params:
+        - filepath (str): filepath output by generate_window_flattened_dfs().
+        - parent_directory (string): the parent directory of the flattened files
+
+        Returns:
+        - dataset_key (string): dataset key that matches to the config and metrics_config
+        """
+        # Remove the parent directory part from the filepath
+        relative_path = filepath.replace(parent_directory, '')
+
+        # Split the remaining path into parts
+        parts = relative_path.split(os.sep)
+
+        # The filename should be the last part
+        filename = parts[-1]
+
+        # Define the date pattern regex
+        date_pattern = re.compile(r'\d{4}-\d{2}-\d{2}_\d{2}-\d{2}')
+
+        # Find the date in the filename
+        date_match = date_pattern.search(filename)
+
+        if date_match:
+            # Extract the part before the date
+            dataset_key = filename[:date_match.start()].rstrip('_')
+            return dataset_key
         else:
-            dataset = dataset_prefix
-
-        # Add the DataFrame to the appropriate group
-        if dataset_prefix not in grouped_dfs:
-
-            grouped_dfs[dataset_prefix] = []
-            # Identify the fill method only once per dataset
-            try:
-                fill_methods[dataset_prefix] = modeling_config['preprocessing']['fill_methods'][dataset]
-            except KeyError as exc:
-                raise KeyError(f"Fill method not found for dataset: {dataset}") from exc
-
-        grouped_dfs[dataset_prefix].append(df)
-
-    # Concatenate DataFrames within each group and pair with fill method
-    concatenated_dfs = {
-        dataset_prefix: (pd.concat(dfs, ignore_index=True), fill_methods[dataset_prefix])
-        for dataset_prefix, dfs in grouped_dfs.items()
-    }
-
-    return concatenated_dfs
-
-
-
-def join_dataset_all_windows_dfs(concatenated_dfs):
-    """
-    Merges the all-windows dataframes of each dataset together according to the fill method
-    specified in the model_config. The param is the format from tw.concat_dataset_time_windows_dfs().
-
-    Params:
-    - concatenated_dfs (Dict[str, Tuple[pd.DataFrame, str]]): A dictionary where keys are
-        dataset prefixes and values are tuples containing:
-            - The concatenated DataFrame for each prefix
-            - The fill method for the dataset
-
-    Returns
-    - training_data_df (DataFrame): df multiindexed on time_window-coin_id, with columns for
-        all flattened features for all datasets.
-    - join_log_df (DataFrame): df that contains briefs logs of join methods and outcomes
-    """
-    # List to store all combinations
-    all_coin_windows = []
-    log_data = []
-
-    # Pull a unique set of all coin_id-time_window pairs across all dfs
-    for dataset, (df, _) in concatenated_dfs.items():
-        if 'coin_id' in df.columns and 'time_window' in df.columns:
-            combinations = df[['time_window', 'coin_id']].drop_duplicates()
-            all_coin_windows.append(combinations)
-
-    # Concatenate all combinations and remove duplicates
-    training_data_df = pd.concat(all_coin_windows, ignore_index=True).drop_duplicates()
-    training_data_df = training_data_df.set_index(['time_window', 'coin_id'])
-
-    # Iterate through df_list and merge each one
-    for dataset, (df, fill_method) in concatenated_dfs.items():
-        rows_start = len(training_data_df)
-
-        # Get columns to track from current df so we can fill them appropriately
-        merge_cols = df.columns.difference(['time_window', 'coin_id'])
-
-        if fill_method == 'retain_nulls':
-            df = df.set_index(['time_window', 'coin_id'])
-            training_data_df = training_data_df.join(df, on=['time_window', 'coin_id'], how='left')
-
-        elif fill_method == 'drop_records':
-            df = df.set_index(['time_window', 'coin_id'])
-            df = df.dropna()
-            training_data_df = training_data_df.join(df, on=['time_window', 'coin_id'], how='inner')
-
-        elif fill_method == 'fill_zeros':
-            df = df.set_index(['time_window', 'coin_id'])
-            training_data_df = training_data_df.join(df, on=['time_window', 'coin_id'], how='left')
-            # Only fill the columns from current df
-            training_data_df[merge_cols] = training_data_df[merge_cols].fillna(0)
-
-        elif fill_method == 'extend_coin_ids':
-            df = df.set_index('time_window')
-            training_data_df = training_data_df.join(df, on='time_window', how='inner')
-
-        elif fill_method == 'extend_time_windows':
-            df = df.set_index('coin_id')
-            training_data_df = training_data_df.join(df, on='coin_id', how='inner')
-
-        else:
-            raise ValueError(f"Invalid fill method '{fill_method}' found in config.yaml.")
-
-        rows_end = len(training_data_df)
-        log_data.append({
-            'dataset': dataset,
-            'fill_method': fill_method,
-            'rows_start': rows_start,
-            'rows_end': rows_end,
-            'rows_removed': rows_start - rows_end
-        })
-
-    join_log_df = pd.DataFrame(log_data)
-
-    return training_data_df, join_log_df
-
-
-
-
-def extract_dataset_key_from_filepath(filepath, parent_directory):
-    """
-    Helper function for concat_dataset_time_windows_dfs().
-
-    Extracts the dataset key (e.g. 'time_series', 'wallet_cohorts', etc) from the
-    flattened filepath.
-
-    Params:
-    - filepath (str): filepath output by generate_window_flattened_dfs().
-    - parent_directory (string): the parent directory of the flattened files
-
-    Returns:
-    - dataset_key (string): dataset key that matches to the config and metrics_config
-    """
-    # Remove the parent directory part from the filepath
-    relative_path = filepath.replace(parent_directory, '')
-
-    # Split the remaining path into parts
-    parts = relative_path.split(os.sep)
-
-    # The filename should be the last part
-    filename = parts[-1]
-
-    # Define the date pattern regex
-    date_pattern = re.compile(r'\d{4}-\d{2}-\d{2}_\d{2}-\d{2}')
-
-    # Find the date in the filename
-    date_match = date_pattern.search(filename)
-
-    if date_match:
-        # Extract the part before the date
-        dataset_key = filename[:date_match.start()].rstrip('_')
-        return dataset_key
-    else:
-        raise ValueError(f"Could not parse dataset of file {filepath}")
+            raise ValueError(f"Could not parse dataset of file {filepath}")
