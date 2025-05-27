d5f3f40b4b31045e32b7815bbecaf2061bffa0c1
tentabs00 <tentabs00@gmail.com>
2025-05-26 20:00:38 -0700

    style: logs

--- src/coin_modeling/coin_epochs_orchestrator.py
+++ src/coin_modeling/coin_epochs_orchestrator.py
@@ -233,7 +233,8 @@ class CoinEpochsOrchestrator:
 
         # 3. Generate all coin epochs' training data
         # ------------------------------------------
-        logger.milestone("Beginning generation of coin model training data...")
+
+        logger.milestone("","Beginning generation of coin model training data...")
         u.notify('whoosh_boom')
 
         # Tag each DataFrame with the epoch date
